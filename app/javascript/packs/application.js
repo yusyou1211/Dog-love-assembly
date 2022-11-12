e06058978951e2846219f65273026f0be05083eb
yusyou1211 <yuya8382718@gmail.com>
2022-11-12 06:53:29 +0000

    config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
 import '@fortawesome/fontawesome-free/js/all';
+window.$ = window.jQuery = require('jquery');
 
 Rails.start();
 Turbolinks.start();
